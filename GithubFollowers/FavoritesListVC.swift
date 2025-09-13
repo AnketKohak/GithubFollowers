638ba9fe70f319ff84e3e7c591b3c11889697b38
AnketKohak <anketkohak40@gmail.com>
2025-09-13 15:30:55 +0530

    Setup of NavigationVC using TabbarVC

---
+++ GithubFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesVC.swift
+//  GithubFollowers
+//
+//  Created by Anket Kohak on 13/09/25.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GithubFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GithubFollowers
+//
+//  Created by Anket Kohak on 13/09/25.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+}
