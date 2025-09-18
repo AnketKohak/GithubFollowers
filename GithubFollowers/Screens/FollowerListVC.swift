8781df3cb87fa2d104a7c6ed5aad4c37f7ea709f
AnketKohak <anketkohak40@gmail.com>
2025-09-18 22:00:12 +0530

    BugFix: NavingationBar

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -12,9 +12,12 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
+        
         navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
 
