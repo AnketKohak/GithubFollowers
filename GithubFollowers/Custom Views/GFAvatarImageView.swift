c61b6fd69c4884c38bf017349bba1cc338ac701c
AnketKohak <anketkohak40@gmail.com>
2025-09-28 22:36:42 +0530

    Follower Cell Created

---
+++ GithubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAvatarImageView.swift
+//  GithubFollowers
+//
+//  Created by Anket Kohak on 27/09/25.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
