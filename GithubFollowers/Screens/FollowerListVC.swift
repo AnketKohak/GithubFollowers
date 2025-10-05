4e36781617c989738e12929472b4ed572648bd9a
AnketKohak <anketkohak40@gmail.com>
2025-10-05 18:49:53 +0530

    memory management and UIHelper

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -9,9 +9,7 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
-    enum Section{
-        case main
-    }
+    enum Section{ case main }
     
     var username: String!
     var followers:[Follower] = []
@@ -28,17 +26,7 @@ class FollowerListVC: UIViewController {
        
     }
     
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout{
-        let width = view.bounds.width
-        let padding:CGFloat = 12
-        let minimumItemSpacing:CGFloat = 10
-        let availableWidth  = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = availableWidth/3
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        return flowLayout
-    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -49,7 +37,7 @@ class FollowerListVC: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     private func configureCollectionView(){
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
@@ -57,12 +45,10 @@ class FollowerListVC: UIViewController {
     }
     
     func getFollower(){
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        NetworkManager.shared.getFollowers(for: username, page: 1) {[weak self] result in
+            guard let self = self else{ return }
             switch result{
             case .success(let followers):
-                print("Followers.count = \(followers.count)")
-                
-                print(followers)
                 self.followers = followers
                 self.updateData()
             case .failure(let error):
