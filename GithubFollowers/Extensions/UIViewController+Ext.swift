e438ab415c63f0381669bc421171651635628d8a
AnketKohak <anketkohak40@gmail.com>
2025-10-09 21:20:18 +0530

    LoaderScreen Added

--- GithubFollowers/Extensions/UIViewController+Ext.swift
+++ GithubFollowers/Extensions/UIViewController+Ext.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
-
+fileprivate var containerView :UIView!
 extension UIViewController{
+   
     func presentGFAlertOnMainThread(title: String,message: String, buttonTitle: String){
         DispatchQueue.main.async {
             let alertVC = GFAlertVC(alertTitle: title, message: message, buttonTitle: buttonTitle)
@@ -15,6 +16,36 @@ extension UIViewController{
             alertVC.modalTransitionStyle = .coverVertical
             self.present(alertVC, animated: true)
             
+        }
+    }
+    
+    func showLoadingView(){
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        
+        UIView.animate(withDuration: 0.25)
+        {
+            containerView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor)
+        ])
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView(){
+        DispatchQueue.main.async{
+            containerView.removeFromSuperview()
+            containerView = nil
         }
     }
 }
